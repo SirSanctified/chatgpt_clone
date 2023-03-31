dafa99c870e4b9ace99ffbddf84bfdc810115bf6
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-31 20:48:47 +0200

    Add live backend url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-clone-bdmi.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
